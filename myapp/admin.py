2cc9b5842372ade7bd2f32ce1d2f30191d293384
ranasalman3838 <ranasalmanahmed38@gmail.com>
2021-07-30 11:09:05 +0500

    2nd commit of adding models in admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+# Register your models her
+class employeeset(admin.ModelAdmin):
+    list_display = ('first_name','last_name','email','gender','office')
+class officeset(admin.ModelAdmin):
+    list_display = ('name','location')
+
+admin.site.register(Employee,employeeset)
+admin.site.register(Office,officeset)
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Office(models.Model):
+    name = models.CharField(max_length=30)
+    location = models.CharField(max_length=30)
+
+class Employee(models.Model):
+    genders=[
+        ("M","Male"),
+        ("F","Female")
+    ]
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    gender= models.CharField(max_length=30,choices=genders)
+    office=models.ForeignKey(Office,on_delete=models.CASCADE)
+
